1fcaf35badc27983774bc8dedee1dd189e088988
Shaheenhiremani <124365463+Shaheenhiremani@users.noreply.github.com>
2024-03-13 00:21:45 +0530

    Add files via upload

---
+++ prime or not.c
@@ -0,0 +1,35 @@
+//Program to Check Whether a Number is Prime or Not
+#include <stdio.h>
+
+int main() {
+
+ int num, i, flag = 0;
+
+ printf(“Enter a number: “);
+
+ scanf(“%d”, &num);
+
+ for (i = 2; i <= num / 2; ++i) {
+
+ if (num % i == 0) {
+
+ flag = 1;
+
+ break;
+
+ }
+
+ }
+
+ if (flag == 0)
+
+ printf(“%d is a prime number.\n”, num);
+
+ else
+
+ printf(“%d is not a prime number.\n”, num);
+
+ return 0;
+
+ }
+
